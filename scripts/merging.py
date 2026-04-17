68538d4ab3efa926ca2f355eabb404106372e257
lyonstran <lyonstran21@gmail.com>
2026-04-17 17:16:05 -0500

    saved merged datasets to new csv

--- scripts/merging.py
+++ scripts/merging.py
@@ -8,4 +8,7 @@ chicago_weather_monthly = pd.read_csv("cleaned_data\chicago_weather_monthly.csv"
 merged = chicago_cta_monthly.merge(chicago_weather_monthly, on = "month", how = "left")
 merged = merged.merge(chicago_gas_price_monthly, on = "month", how = "left")
 
+# save data to csv
+merged.to_csv("merged_datasets.csv", index = False)
+
 
